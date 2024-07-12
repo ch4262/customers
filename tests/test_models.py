6865fc6298b7beeaa19fabe4c2daa041a3c7f084
Sai Preetham Bojja <bojjasaipreetham@gmail.com>
2024-07-12 11:25:01 +0530

    Fixed linting error

--- tests/test_models.py
+++ tests/test_models.py
@@ -46,36 +46,9 @@ class TestCaseBase(TestCase):
         """This runs after each test"""
         db.session.remove()
 
-
-######################################################################
-#  C U S T O M E R   M O D E L   T E S T   C A S E S
-######################################################################
-# pylint: disable=too-many-public-methods
-class TestCustomer(TestCase):
-    """Test Cases for Customer Model"""
-
-    @classmethod
-    def setUpClass(cls):
-        """This runs once before the entire test suite"""
-        app.config["TESTING"] = True
-        app.config["DEBUG"] = False
-        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-        app.logger.setLevel(logging.CRITICAL)
-        app.app_context().push()
-
-    @classmethod
-    def tearDownClass(cls):
-        """This runs once after the entire test suite"""
-        db.session.close()
-
-    def setUp(self):
-        """This runs before each test"""
-        db.session.query(Customer).delete()  # clean up the last tests
-        db.session.commit()
-
-    def tearDown(self):
-        """This runs after each test"""
-        db.session.remove()
+    ######################################################################
+    #  C U S T O M E R   M O D E L   T E S T   C A S E S
+    ######################################################################
 
     ######################################################################
     #  T E S T   C A S E S
